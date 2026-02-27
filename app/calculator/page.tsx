88b2f1f4b612b3ec811c209235d750e050cf7e05
SaMuii72 <113111313+SaMuii72@users.noreply.github.com>
2026-02-27 15:31:41 +0700

    Result Page

---
+++ app/calculator/page.tsx
@@ -0,0 +1,9 @@
+import MayanCalculator from "@/components/MayanCalculator";
+
+export default function CalculatorPage() {
+  return (
+    <main className="min-h-screen bg-black text-white px-6 py-24 flex justify-center">
+      <MayanCalculator />
+    </main>
+  );
+}
